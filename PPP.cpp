bc906e1131786b26e850b393c2c6f96cf58a7a97
bs-code23 <90001972+bs-code23@users.noreply.github.com>
2021-11-11 23:44:27 -0600

    Update PPP.cpp
    oh god why do you give me your toughest battles

--- PPP.cpp
+++ PPP.cpp
@@ -3,10 +3,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <time.h>
-
-
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
+
+/**
+Brian Shin + Samuel Adams
+COMP 3270 - Yilmaz, Levent
+Programming Assignment: MSCS Problem
+
+Compile and run with C++ (mingw-w64\\x86_64-8.1.0-posix-seh-rt_v6-rev0)
+IDE used: Visual Studio Code
+
+References used:
+c++ for vector
+reuse of code from COMP 2710 for reading files
+stack overflow for delimiting commas in an input file
+stack overflow for measuring execution time
+
+Certification statement: I certify that I wrote the code I am submitting. I did not copy whole or parts of it from another student or have another person write the code for me.
+Any code I am reusing in my program is clearly marked as such with its source clearly identified in comments. 
+**/
 
 // function declarations
 int algorithm1(vector<int>& X);
@@ -14,111 +31,107 @@ int algorithm2(vector<int>& X);
 int algorithm3(vector<int>& X, int L, int U);
 int algorithm4(vector<int>& X);
 
-// reading/writing/checking file functions
-
+// helper function declarations
 bool check_file(string);
-vector<int> read_file(string);  // i want this to read the file and also store the integers into the vector as well
-void writefile(vector<int>& X, int outputArray_size, string file);
+vector<int> read_file(string);
+
+// input file
 string file = "phw_input.txt";
 
 // main method
 int main() {
+    // verify file, pass it as an array for use and outputs MSCS of array
     check_file(file);
-
     vector<int> readArray = read_file(file);
-    for (int i : readArray) {
-        cout << i << "\n";
-    }
-    cout <<"algorithm 1: " << algorithm1(readArray) << "\n";
-
+    cout << "algorithm 1: " << algorithm1(readArray) << "\n";
     cout << "algorithm 2: " << algorithm2(readArray) << "\n";
-    
     cout << "algorithm 3: " << algorithm3(readArray, 0, readArray.size() - 1) << "\n";
-
     cout << "algorithm 4: " << algorithm4(readArray) << "\n";
 
-    vector<vector<int>> randommatrix;
-
+    // creating input matrix
+    vector<vector<int>> matrix;
+    // created 19 rows of differing sizes
     for (int i = 0; i < 19; i++) {
-        vector<int> column;
-
+        vector<int> row;
+        // makes each row's size and pushing random integers into each available spot from 5-100
         for (int j = 0; j < (i + 2) * 5; j++) {
-            column.push_back(rand() % 100 + 1);
-
+            row.push_back(rand() % 100 + 1);
         }
-        randommatrix.push_back(column);
+        // filling each row of 2d matrix with random integers
+        matrix.push_back(row);
     }
-    // get Empirical time
-    double t1 = 0, t2 = 0, t3 = 0, t4 = 0;
-    int N = 1000000;
+
+    // testing runtime execution using system clock
     vector<vector<int>> timeMatrix;
-
+    int N = 100000;
+    double t1 = 0, t2 = 0, t3 = 0, t4 = 0;
     for (int i = 0; i < 19; i++) {
-
+        // honestly forgot why this is here
+        // vector<double> result;
         for (int j = 0; j < 8; j++) {
+            for (int k = 0; k < N; k++) {
+                t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+                
+                // Algo 1 runtime
+                auto start = high_resolution_clock::now();
+                algorithm1(matrix.at(i));
+                auto stop = high_resolution_clock::now();
+                auto duration = duration_cast<milliseconds>(stop - start);
+                t1 += duration.count();
 
-            for (int k = N; k < N; k++) {
-                t1 = 0;
-                t2 = 0;
-                t3 = 0;
-                t4 = 0;
+                // Algo 2 runtime
+                /**
+                start = high_resolution_clock::now();
+                algorithm2(matrix.at(i));
+                stop = high_resolution_clock::now();
+                duration = duration_cast<milliseconds>(stop - start);
+                t2 += duration.count();
+                **/
 
-                //start clock
-                // alg one
+                // Algo 3 runtime
+                /**
+                start = high_resolution_clock::now();
+                algorithm3(matrix.at(i), 0, matrix.at(i).size() - 1);
+                stop = high_resolution_clock::now();
+                duration = duration_cast<milliseconds>(stop - start);
+                t3 += duration.count();
+                **/
+                // Algo 4 runtime
+                /**
+                start = high_resolution_clock::now();
+                algorithm4(matrix.at(i));
+                stop = high_resolution_clock::now();
+                duration = duration_cast<milliseconds>(stop - start);
+                t4 += duration.count();
+                **/
+
+                // alt solution: sam adams
+                /** uses <time.h> library
                 clock_t start = clock();
-              //  double begin = get_cpu_time();
-                algorithm1(randommatrix.at(i));
+                //double begin = get_cpu_time();
+                algorithm1(matrix.at(i));
                 clock_t end = clock();
                 double elapsed = double(end - start) / CLOCKS_PER_SEC;
-               // double end = get_cpu_time();
-              //  double elapsed = (end - begin);
-
+                //double end = get_cpu_time();
+                //double elapsed = (end - begin);
                 t1 = elapsed + t1;
-                cout << elapsed;
-
-                // alg 2
-       /**         start = clock();
-                algorithm2(randommatrix.at(i));
-                end = clock();
-                elapsed = double(end - start) / CLOCKS_PER_SEC;
-                t2 = elapsed + t2;
-
-                // alg 3
-               start = clock();
-                algorithm3(randommatrix.at(i), 0, randommatrix.at(i).size() - 1);
-                 end = clock();
-                 elapsed = double(end - start) / CLOCKS_PER_SEC;
-                t3 = elapsed + t3;
-
-                //alg 4
-                 start = clock();
-                algorithm4(randommatrix.at(i));
-                 end = clock();
-                 elapsed = double(end - start) / CLOCKS_PER_SEC;
-                t4 = elapsed + t4; **/
-
-
+                **/
             }
-
-            t1 = t1 / N;
-            t2 = t2 / N;
-            t3 = t3 / N;
-            t4 = t4 / N;
-
+            // finding averages
+            t1 /= N;
+            //t2 /= N;
+            //t3 /= N;
+            //t4 /= N;
             cout << t1 << "\n";
-
-
-
         }
-        
     }
-
+    return 0;
 }
 
 // function/algorithm definitions
 // each algorithm is a solution to MSCS
 
-// T(n) = 
+// alg 1
 int algorithm1(vector<int>& X) {
     int maxSoFar = 0;
     int P = 0;
@@ -136,7 +149,7 @@ int algorithm1(vector<int>& X) {
     return maxSoFar;
 }
 
-// #2
+// alg 2
 int algorithm2(vector<int>& X) {
     int maxSoFar = 0;
     int P = 0;
@@ -200,6 +213,7 @@ int algorithm4(vector<int>& X) {
     return maxSoFar;
 }
 
+
 // helper functions
 
 bool check_file(string file) {
@@ -215,7 +229,6 @@ bool check_file(string file) {
 }
 
 vector<int> read_file(string fileIn) {
-   // int i = 0;
     ifstream stream;
     vector<int> X;
     int index;
@@ -224,32 +237,5 @@ vector<int> read_file(string fileIn) {
     while (getline(stream, number_as_string, ',')) {
         X.push_back(stoi(number_as_string));
     }
-    /**for (vector<int>::iterator it = X.begin(); it != X.end(); ++it) {
-        cout << ' ' << *it;
-        cout << '\n';
-    } **/
     return X;
 }
-
-
-void writefile(vector<int>& X, int outputArray_size, string file) {
-    ofstream outstream;
-    string output_filename;
-    cout << "Enter the output file name: ";
-    cin >> output_filename;
-
-    outstream.open((char*)output_filename.c_str());
-    for (int i = 0; i < outputArray_size; i++) {
-        outstream << X[i] << "\n";
-    }
-    outstream.close();
-
-    cout << "*** Please check the new file - "
-        << output_filename << " ***\n"
-        << "*** Goodbye. ***";
-}
-
-
-
-
-
